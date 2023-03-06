b993e6a45552cbda4b100cc509509eebc2f4053a
Alexandr Dylevich <dylevich220885@gmail.com>
2023-03-06 22:08:12 +0300

    added EditableSpan.tsx component for editing task title (or todolist title) after double click  (lesson 6 (tuesday-02))

---
+++ src/components/EditableSpan.tsx
@@ -0,0 +1,48 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+
+export type EditableSpanPropsType = {
+    title: string
+    onChange: (newTitleValue: string) => void
+    className?: string
+
+}
+export const EditableSpan = (props: EditableSpanPropsType) => {
+    let [editMode, setEditMode] = useState<boolean>(false)//стейт для хранения режима отображения (просмотр-ViewMode или редактирование-EditMode)
+    let [title, setTitle] = useState<string>('') //стейт для хранения вводимого значения (перезаписывается при onChange)
+
+    const activateViewMode = () => {
+        setEditMode(false)
+        props.onChange(title)
+    }
+    const activateEditMode = () => {
+        setEditMode(true)
+        setTitle(props.title)
+    }
+    const onChangeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(event)
+        setTitle(event.currentTarget.value)
+    }
+    let onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>)=>{
+        //console.log(event)
+        // if((event.code==="Enter"||event.code==="NumpadEnter") && event.ctrlKey)//так тоже работает
+        if(event.key==="Enter"){
+            activateViewMode()
+        }
+    }
+    return (
+        editMode
+            ?
+            <input
+                type="text"
+                onChange={onChangeTitleHandler}
+                onKeyDown={onKeyDownHandler}
+                value={title}
+                onBlur={activateViewMode}
+                autoFocus={true}
+            />
+            :
+            <span className={props.className || ''} onDoubleClick={activateEditMode}>
+                {props.title}
+            </span>
+    )
+}
